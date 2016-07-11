195223f25283a7646e9a2ab4e845fe4ab24a328e
rin <rindrm@gmail.com>
2016-07-12 01:34:05 +0900

    160712/01:23 update Top.html

--- web/ui.js
+++ web/ui.js
@@ -33,3 +33,39 @@
     };
 
 }(this, this.document));
+
+
+  var opa = 0; //透明度
+  var opacnt = 3; //増加割合
+  var timer = 100; //setTimeout関数の実行感覚(ms)
+
+function changeIMG(element){
+  var img = new Array();
+
+  img[1] = new Image();
+  img[1].src = "photo/1.png";
+  img[2] = new Image();
+  img[2].src = "photo/2.png";
+  img[3] = new Image();
+  img[3].src = "photo/3.png";
+  img[4] = new Image();
+  img[4].src = "photo/4.png";
+  img[5] = new Image();
+  img[5].src = "photo/5.png";
+
+  var getid = element.id;
+
+  MainIMG = document.getElementById("IMG");
+  MainIMG.src = img[getid].src;
+  //MainIMG.style.visibility = "visible";
+  //FadeIn(getid,opa);
+}
+//実装中
+/*function FadeIn(getid,opa){
+  if(opa<=100){
+    MainIMG.style.filter = "alpha(opacity:"+opa+")";//IE source
+    MainIMG.style.opacity = opa/100;//Firefox source
+    opa += opacnt;
+    setTimeout("FadeIn('"+getid+"',"+opa+")",timer);
+  }
+}*/
